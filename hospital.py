9945e1b5bdfc16523bc892bff0e2d5d7bc916bdc
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-03-10 00:37:53 -0300

    :hammer: Ajustes na classe Hospital

--- hospital.py
+++ hospital.py
@@ -1,6 +1,6 @@
+from medico import Medico
 class Hospital:
-    def __init__(self, nome, quartos=[], medicos=[], pacientes=[]):
-        self.nome = nome
+    def __init__(self, quartos=[], medicos=[], pacientes=[]):
         self.quartos = quartos
         self.medicos = medicos
         self.pacientes = pacientes
@@ -11,38 +11,36 @@ class Hospital:
         if not medicos:
             print('Não há médicos cadastrados no sistema. Por favor, cadastre um médico. \n')
         else:
-            for i in range(len(medicos)):
-                print(f'MÉDICO {i+1}: ')
+            for i in range(0, len(medicos), 1):
                 medicos[i].ver_medico()
+
 
     def ver_quartos(self):
         quartos = self.quartos
         if not quartos:
             print('Não há quartos cadastrados no sistema. Por favor, cadastre um quarto. \n')
         else:
-            for i in range(len(quartos)):
-                print(f'Quarto {i}: {quartos[i]}')
+            for i in range(0, len(quartos), 1):
+                quartos[i].ver_quarto()
 
-    def get_medico(self, id):
-        for medico in medicos:
-            if medico.get_id() == id:
-                return medico;
 
     def add_medico(self, medico):
         self.medicos.append(medico)
 
-    def delete_medico(self, medico):
-        self.medicos.remove(medico)
+    def add_quarto(self, medico):
+        self.quartos.append(medico)
 
+    def add_paciente(self, paciente):
+        self.pacientes.append(paciente)
 
     def get_medicos(self):
         return self.medicos
 
     def get_quartos(self):
-        return self.quartos
+        return self.medicos
 
-    def get_name(self):
-        return self.nome
+    def get_medico(self, id):
+        for medico in self.medicos:
+            if medico.get_id() == id:
+                return medico;
 
-    def set_name(self, nome):
-        self.nome = nome
