7b2e0a156ac0df08379747018a85c5a9b6632478
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-03-10 00:39:20 -0300

    :construction: Ajustes na classe Médico

--- medico.py
+++ medico.py
@@ -1,19 +1,14 @@
 from usuario import  Usuario
 
 class Medico(Usuario):
-    def __init__(self, id, nome, idade, sexo, cpf, rg, cep, crm, especialidade):
-        super().__init__(id, nome, idade, sexo, cpf, rg, cep)
+    def __init__(self, id, nome, idade, sexo, cpf, rg, cep, email, crm, especialidade):
+        super().__init__(id, nome, idade, sexo, cpf, rg, cep, email)
         self.crm = crm
         self.especialidade = especialidade
 
     def ver_medico(self):
-        print (f'Nome: {self.nome} \n'
-               f'Idade: {self.idade} \n'
-               f'Sexo: {self.sexo} \n'
-               f'CPF: {self.cpf} \n'
-               f'RG: {self.rg} \n'
-               f'CEP: {self.cep} \n'
-               f'CRM: {self.crm} \n'
+        print (f'MÉDICO {self.id} \n'
+               f'Nome: {self.nome} \n'
                f'Especialidade: {self.especialidade} \n')
 
 
