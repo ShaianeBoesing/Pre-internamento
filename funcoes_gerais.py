b109ce8fd54a0d7c9b96156cc434a0b4198ad10d
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-03-10 00:40:54 -0300

    :construction: Criação de documento para incluir funções gerais que serão chamadas pela main function

---
+++ funcoes_gerais.py
@@ -0,0 +1,92 @@
+import interface
+from time import sleep
+from hospital import Hospital
+from quarto import Quarto
+from medico import Medico
+from paciente import Paciente
+
+
+
+def criar_hospital():
+    hospital = Hospital()
+    criar_medicos(hospital)
+    criar_quartos(hospital)
+
+    return hospital
+
+def criar_medicos(hospital):
+    id = 1
+    medico1 = Medico(id, 'João', '45', 'M', '14785296312', '123456789', '99705488', 'joao@email.com',
+                         '987654321', 'pediatria')
+    id += 1
+    medico2 = Medico(id, 'Paulo', '33', 'M', '98756423125', '856789654', '99705488', 'paulo@email.com',
+                             '369852147', 'cardiologia')
+
+    id += 1
+    medico3 = Medico(id, 'Fernanda', '29', 'F', '753951486', '159789523', '99705488',
+                             'fernanda@email.com', '321654987', 'ortopedia')
+    id += 1
+    hospital.add_medico(medico1)
+    hospital.add_medico(medico2)
+    hospital.add_medico(medico3)
+
+def criar_quartos(hospital):
+    quarto1 = Quarto(1, 'Livre')
+    quarto2 = Quarto(2, 'Livre')
+    quarto3 = Quarto(3, 'Livre')
+    quarto4 = Quarto(4, 'Ocupado')
+
+    hospital.add_quarto(quarto1)
+    hospital.add_quarto(quarto2)
+    hospital.add_quarto(quarto3)
+    hospital.add_quarto(quarto4)
+
+def cadastrar_paciente(hospital):
+    nome = interface.leiaString('Nome: ')
+    idade = interface.leiaInt('Idade: ')
+    sexo = interface.leiaString('Sexo: ')
+    cpf = interface.leiaString('CPF: ')
+    rg = interface.leiaString('RG: ')
+    cep = interface.leiaString('CEP: ')
+    email = interface.leiaString('E-mail: ')
+    dataInternacao = interface.leiaString('Data de Internação: ')
+    dataNascimento = interface.leiaString('Data de Nascimento: ')
+    alergia = interface.leiaString('Alergia (Digite "N" caso não tenha alergia): ')
+    nacionalidade = interface.leiaString('Nacionalidade: ')
+    naturalidade = interface.leiaString('Naturalidade: ')
+    nomePai = interface.leiaString('Nome do Pai: ')
+    nomeMae = interface.leiaString('Nome da Mãe: ')
+    endereco = interface.leiaString('Endereço: ')
+    numEndereco = interface.leiaString('Número da residência: ')
+    complemento = interface.leiaString('Complemento: ')
+    bairro = interface.leiaString('Bairro: ')
+    cidade = interface.leiaString('Cidade: ')
+    estado = interface.leiaString('Estado: ')
+    pais = interface.leiaString('País: ')
+    telefone = interface.leiaString('Telefone: ')
+    nomeAcompanhante = interface.leiaString('Nome do Acompanhante: ')
+    telefoneAcompnhante = interface.leiaString('Telefone do Acompanhante: ')
+    estadocivil = interface.leiaString('Estado Civil: ')
+    religiao = interface.leiaString('Religião: ')
+    profissao = interface.leiaString('Profissão: ')
+    empresa = interface.leiaString('Empresa: ')
+    pagamento = interface.leiaString('Salário: ')
+    print(interface.linha())
+    print('Buscando Médicos... ')
+    sleep(1)
+    hospital.ver_medicos()
+    medico = hospital.get_medico(interface.leiaInt('Código do Médico desejado: '))
+    print(interface.linha())
+    print('Buscando Quartos... ')
+    sleep(1)
+    hospital.ver_quartos()
+    quarto = hospital.get_medico(interface.leiaInt('Número do Quarto desejado: '))
+
+    paciente = Paciente(id, nome, idade, sexo, cpf, rg, cep, email,
+                    dataInternacao, dataNascimento,alergia,
+                    nacionalidade,naturalidade,nomePai,nomeMae,
+                    endereco,numEndereco,complemento,bairro,cidade,estado,pais,telefone,
+                    nomeAcompanhante,telefoneAcompnhante,
+                    estadocivil,religiao,profissao,empresa,pagamento,medico, quarto)
+
+    hospital.add_paciente(paciente)
