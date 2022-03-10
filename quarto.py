6805f79cb4a20ab2f41cf7353891a5e1dfd00dcc
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-03-10 00:37:13 -0300

    :construction: Implementação da classe Quarto

---
+++ quarto.py
@@ -0,0 +1,25 @@
+from paciente import Paciente
+
+class Quarto:
+    def __init__(self, numero, situacao):
+        self.numero = numero
+        self.situacao = situacao
+        self.paciente = None
+
+    def get_situacao(self):
+        return self.situacao
+
+    def get_paciente(self):
+        return self.paciente
+
+    def set_situacao(self, situacao):
+        self.situacao = situacao
+
+    def set_paciente(self, paciente):
+        self.paciente = paciente
+
+    def ver_quarto(self):
+        if self.situacao == 'Livre':
+            print (f'QUARTO {self.numero}')
+
+
